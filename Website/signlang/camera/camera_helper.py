ab632e85ddfc0745abb7dc8b562692c05a73ed3f
Param Shendekar <paramvs8@gmail.com>
2021-04-18 18:13:52 +0530

    Final Logic Without Ajax

--- Website/signlang/camera/camera_helper.py
+++ Website/signlang/camera/camera_helper.py
@@ -12,14 +12,24 @@ class WebCam(object):
 
     def get_frame(self):
         status, image = self.video.read()
+
+        #Flip Image
         image_flip = cv2.flip(image, 1)
+
+        #Crop Image
         x, y, w, h = 375, 100, 245, 260
         cv2.rectangle(image_flip, (x, y), (x + w, y + h), (0, 255, 0), 3)
         crop_img = image_flip[y:y + h, x:x + w]
-        self.img_list.append(crop_img)
         ret, jpeg = cv2.imencode('.jpg', image_flip)
+
+        #Image Path
         path = os.getcwd() + "\camera\images" + "\image{}.jpg".format(self.counter)
+        self.img_list.append(path)
         self.counter += 1
-        print(path)
-        cv2.imwrite(path, crop_img)
-        return jpeg.tobytes() 
+        if self.counter==10:
+            self.counter=0
+        #Save Image
+        flipped_crop_img = cv2.flip(crop_img, 1)
+        cv2.imwrite(path, flipped_crop_img)
+
+        return jpeg.tobytes(), flipped_crop_img
--- Website/signlang/camera/views.py
+++ Website/signlang/camera/views.py
@@ -1,21 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import StreamingHttpResponse
 from camera.camera_helper import WebCam
 import time
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+import statistics
+from statistics import mode
+  
+def most_common(List):
+    return(mode(List))
+
+wordFlag = 0
+
+SIZE = 64, 64
+PATH = os.getcwd() + "\camera\Alphabet_Classifier__Preprocess_Lite.tflite"
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(PATH)
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+predictedCharacter = '*'
+
+map_idx_to_char = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F',
+                   6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+                   12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R',
+                   18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X',
+                   24: 'Y', 25: 'Z', 26: 'del', 27: 'nothing', 28: ' ',
+                   29: 'other'}
 
 def index(request):
-	context = {'switch':False}
+	""" context = {
+		'switch': False,
+		'predictedCharacter': predictedCharacter
+	}
 	if request.method=='GET':
 		if 'btn' in request.GET:
 			switch = request.GET['btn']
 			if switch=='turn-on':
-				context['switch'] = True
-	return render(request, 'index.html', context)
+				context['switch'] = True """
+	return render(request, 'index.html')
 
 def gen(camera):
-	end_time = time.time() + 5
-	while time.time() < end_time:
-		frame = camera.get_frame()
+	freq = []
+	predictedString = ''
+	nothingCtr = 0
+	while True:
+		frame, image = camera.get_frame()
+		predictedCharacter = prediction(image)
+		if predictedCharacter!='nothing':
+			nothingCtr = 0
+			freq.append(predictedCharacter)
+			print(predictedCharacter)
+			#wordFlag = 1
+		else:
+			nothingCtr += 1
+			if nothingCtr==250:
+				break
+			if freq!=[]:
+				maxChar = most_common(freq)
+				predictedString += maxChar
+				freq = []
+				print(predictedString)
 		yield (b'--frame\r\n'
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 	
@@ -25,5 +75,22 @@ def video_feed(request):
 					content_type='multipart/x-mixed-replace; boundary=frame')
 
 def tester(request):
-	return render(request, 'tester.html')
-	#saved_model = tf.keras.models.load_model('Alphabet Classifier With Preprocessing')
+	return render(request, 'tester.html?btn=turn-off')
+
+def prediction(image):
+	
+	#Preprocessing
+	resizedImage = cv2.resize(image, SIZE)
+	input_data = np.array(resizedImage, dtype = np.float32)
+	input_data = input_data[np.newaxis, ...]
+	interpreter.set_tensor(input_details[0]['index'], input_data)
+
+	interpreter.invoke()
+
+	# The function `get_tensor()` returns a copy of the tensor data.
+	# Use `tensor()` in order to get a pointer to the tensor.
+	output_data = interpreter.get_tensor(output_details[0]['index'])
+	idx = np.argmax(output_data, axis=-1)
+	prediction = map_idx_to_char.get(idx[0])
+
+	return prediction
